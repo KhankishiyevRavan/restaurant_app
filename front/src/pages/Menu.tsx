7f75e306182497a50c97b02a9456bfe03166e6e2
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2025-08-04 17:53:05 +0400

    upload

--- front/src/pages/Menu.tsx
+++ front/src/pages/Menu.tsx
@@ -86,7 +86,7 @@ export default function Menu() {
         </div>
 
         {/* Menu Items */}
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
           {filteredItems.map((item) => (
             <div
               key={item._id}
