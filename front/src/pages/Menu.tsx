cdf9017712f0e9a1f3a9bdc1a214d68e12d1d985
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2025-08-04 16:47:54 +0400

    upload

--- front/src/pages/Menu.tsx
+++ front/src/pages/Menu.tsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
-import { Search, Star, ShoppingCart, Heart, CheckCircle } from "lucide-react";
+import { Search, Star, Heart, CheckCircle } from "lucide-react";
 import { getMenuItems } from "../service/menuService";
 import { useTranslation } from "react-i18next";
 
 type MenuItem = {
   _id: string;
-  name?: {
-    az?: string;
-    en?: string;
-    ru?: string;
+  name: {
+    az: string;
+    en: string;
+    ru: string;
   };
-  price?: string;
-  time?: string;
-  rating?: number;
-  image?: string;
-  category?: string;
+  price: string;
+  time: string;
+  rating: number;
+  image: string;
+  category: string;
 };
 
 const categories = ["all", "breakfast", "lunch", "treats", "dessert", "drinks"];
@@ -22,7 +22,7 @@ const categories = ["all", "breakfast", "lunch", "treats", "dessert", "drinks"];
 export default function Menu() {
   const { t, i18n } = useTranslation();
   type LangKey = "az" | "en" | "ru";
-  const currentLang: LangKey = (i18n.language as LangKey) || "az";
+  const currentLang = (i18n.language as LangKey) || "az";
 
   const [activeCategory, setActiveCategory] = useState("all");
   const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
@@ -31,7 +31,6 @@ export default function Menu() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getMenuItems();
-      console.log(data);
       setMenuItems(data);
     };
     fetchData();
@@ -39,10 +38,10 @@ export default function Menu() {
 
   // Filter (category + search)
   const filteredItems = menuItems.filter((item) => {
-    const itemName = item.name?.[currentLang]?.toLowerCase() || "";
+    const itemName = item.name[currentLang]?.toLowerCase() || "";
     return (
       (activeCategory === "all" || item.category === activeCategory) &&
-      itemName.includes(searchTerm.toLowerCase())
+      itemName.includes(searchTerm?.toLowerCase())
     );
   });
 
@@ -95,8 +94,8 @@ export default function Menu() {
               <div className="relative">
                 <img
                   className="w-full h-48 object-cover"
-                  src={`http://localhost:5002${item.image || ""}`}
-                  alt={item.name?.[currentLang] || "No name"}
+                  src={`${import.meta.env.VITE_API_URL}${item.image}`}
+                  alt={item.name[currentLang]}
                 />
                 <button className="absolute top-2 left-2 bg-white rounded-full p-2 shadow-md hover:scale-110 transition">
                   <Heart className="text-red-500 w-5 h-5" />
@@ -107,29 +106,29 @@ export default function Menu() {
               <div className="p-4">
                 <div className="flex justify-between items-center mb-2">
                   <span className="text-red-600 font-bold text-xl">
-                    {item.price || "0"}$
+                    {item.price}$
                   </span>
                   <div className="flex items-center gap-1 text-sm text-gray-600">
                     <Star className="text-yellow-500 w-4 h-4" />
-                    <span className="font-semibold">{item.rating || 0}</span>
+                    <span className="font-semibold">{item.rating}</span>
                   </div>
                 </div>
 
                 <h3 className="font-bold text-lg text-gray-800">
-                  {item.name?.[currentLang] || t("no_name")}
+                  {item.name[currentLang]}
                 </h3>
 
                 <ul className="mt-2 space-y-1 text-gray-600 text-sm">
                   <li className="flex items-center gap-2">
                     <CheckCircle className="text-orange-500 w-4 h-4" />
-                    {item.time || "-"}
+                    {item.time}
                   </li>
                 </ul>
 
-                <button className="mt-4 w-full flex items-center justify-center gap-2 py-2 border rounded-xl text-gray-700 hover:bg-red-500 hover:text-white hover:border-red-500 transition">
+                {/* <button className="mt-4 w-full flex items-center justify-center gap-2 py-2 border rounded-xl text-gray-700 hover:bg-red-500 hover:text-white hover:border-red-500 transition">
                   <ShoppingCart className="w-5 h-5" />
                   {t("add_to_cart")}
-                </button>
+                </button> */}
               </div>
             </div>
           ))}
