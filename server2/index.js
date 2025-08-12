7b94e3740fe5967b1620989f6f8fd5fd65ef62ae
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2025-08-12 10:57:19 +0400

    add login

--- server2/index.js
+++ server2/index.js
@@ -16,7 +16,64 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // şəkil
 
 // DB Connect
 connectDB();
+const createAdminRole = async () => {
+  try {
+    const existingRole = await Role.findOne({ name: "admin" });
+    if (!existingRole) {
+      const adminRole = new Role({
+        name: "admin",
+        permissions: ["create", "edit", "delete"], // Admin icazələri (istəyə uyğun əlavə edə bilərsiniz)
+      });
+      await adminRole.save();
+      console.log("✅ Admin role created");
+    } else {
+      console.log("ℹ️ Admin role already exists");
+    }
+  } catch (err) {
+    console.error("Error creating admin role:", err);
+  }
+};
 
+// createAdminRole();
+
+const createAdminUser = async () => {
+  try {
+    const existingAdmin = await User.findOne({
+      fname: "admin",
+      lname: "admin",
+    });
+
+    // Əgər admin istifadəçisi artıq varsa, məlumat veririk
+    if (!existingAdmin) {
+      // Admin rolunu tapırıq
+      const adminRole = await Role.findOne({ name: "admin" });
+
+      // Admin rolunu tapmaq olmazsa, xəbərdarlıq veririk
+      if (!adminRole) {
+        console.log("Error: Admin role not found");
+        return;
+      }
+
+      // Yeni admin istifadəçisi yaradılır
+      const admin = new User({
+        fname: "admin",
+        lname: "admin",
+        email: process.env.ADMIN_EMAIL,
+        password: process.env.ADMIN_PASS,
+        role: adminRole._id, // Admin rolunun ObjectId-si istifadəçiyə təyin edilir
+      });
+
+      await admin.save();
+      console.log("✅ Admin user created");
+    } else {
+      console.log("ℹ️ Admin user already exists");
+    }
+  } catch (err) {
+    console.error("Error creating admin user:", err);
+  }
+};
+
+// createAdminUser();
 // Routes
 const menuRoutes = require("./routes/menuRoutes");
 app.use("/api/menu", menuRoutes);
